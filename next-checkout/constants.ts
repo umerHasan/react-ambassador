68cda1f1f113d188cc7330c9a96d5bf38e960e71
Muhammad Umer /Tech/ Convex Interactive <muhammad.umer@convexinteractive.com>
2024-08-19 16:08:01 +0500

    configuration setup

--- next-checkout/constants.ts
+++ next-checkout/constants.ts
@@ -1,4 +1,5 @@
 export default {
-    endpoint: 'http://localhost:8000/api/checkout',
-    stripe_key: 'pk_test_51H0wSsFHUJ5mamKON9UhPL8Rws5mF3p5aTqKK9kh2Rvk0DnQcpSLYujzQoWqTyp02tkWjYB8o9YRKSFWThARxyT100uqDDnoVy'
-}
+  endpoint: "http://localhost:8000/api/checkout",
+  stripe_key:
+    "pk_test_51KQqQdRbrxhlYgxxOqGEPI7JX5ypkSfvXUMKolACJQfAmM3flCHRsXvO7qmKKfMFmtetPU6awYNIOim8pUpc5PJe00tuhYGvFQ",
+};
